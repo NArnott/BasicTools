91ea06add8c0659be2d00ed5f22ec3d4369eb09a
Nathan Arnott <nathan@plainlogic.net>
2021-07-30 18:02:37 -0600

    Add project files.

---
+++ SimpleDevToolbox/Client/Pages/Guids.razor.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
+using SimpleDevToolbox.Client.Support;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace SimpleDevToolbox.Client.Pages
+{
+    partial class Guids
+    {
+        private readonly CreateGuidModel _createGuidModel = new();
+        private readonly EditContext _editContext;
+        
+        [Inject]
+        IJSRuntime JSRuntime { get; set; }
+        
+        private GuidModel[] _guids;
+
+        public Guids()
+        {
+            _editContext = new EditContext(_createGuidModel);
+            _editContext.SetFieldCssClassProvider(new BootstrapCssClassProvider());
+        }
+
+        private void CreateGuids()
+        {
+            _guids = new GuidModel[_createGuidModel.CreateGuidCount];
+
+            for (int i = 0; i < _createGuidModel.CreateGuidCount; i++)
+            {
+                _guids[i] = new GuidModel();
+            }
+
+            _editContext.MarkAsUnmodified();
+
+            //await JSRuntime.InvokeVoidAsync("navigator.clipboard.writeText", _guids[0].ToString());
+        }
+
+        public async Task SelectGuid(GuidModel model)
+        {
+            foreach (var guid in _guids) guid.IsActive = false;
+
+            model.IsActive = true;
+
+            await JSRuntime.InvokeVoidAsync("navigator.clipboard.writeText", model.Value.ToString());
+        }
+    }
+
+    public class GuidModel
+    {
+        public Guid Value { get; } = Guid.NewGuid();
+
+        bool _isActive;
+
+        public bool IsActive
+        {
+            get => _isActive;
+            set
+            {
+                _isActive = value;
+
+                if (value)
+                    _wasActive = true;
+            }
+        }
+
+        bool _wasActive;
+
+        public bool WasActive => _wasActive && !_isActive;
+    }
+
+    public class CreateGuidModel
+    {
+        [Required]
+        [Range(1, 250)]
+        public int CreateGuidCount { get; set; } = 1;
+    }
+}
---
+++ SimpleDevToolbox/Client/Support/BootstrapCssClassProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDevToolbox.Client.Support
+{
+    class BootstrapCssClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            bool isInvalid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            if (!isInvalid)
+            {
+                return "is-invalid";
+            }
+
+            if (editContext.IsModified(in fieldIdentifier))
+            {
+                return "is-valid";
+            }
+
+            return string.Empty;
+        }
+    }
+}
