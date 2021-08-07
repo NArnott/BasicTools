e9342fc7fb4a3e77030f77de0bbf50bc9b14c24f
Nathan Arnott <nathan@plainlogic.net>
2021-08-06 20:13:55 -0600

    rework icons

---
+++ BasicTools.Client/Shared/Icon.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BasicTools.Client.Shared
+{
+    partial class Icon
+    {
+
+    }
+
+    public enum IconStyles
+    {
+        Filled,
+        Outlined,
+        Rounded,
+        Sharp,
+        TwoTone
+    }
+
+    public enum IconSizes : byte
+    {
+        Small = 18,
+        Medium = 24,
+        Large = 36,
+        XLarge = 48
+    }
+}
--- BasicTools.Shared/ToolPageAttribute.cs
+++ BasicTools.Shared/ToolPageAttribute.cs
@@ -5,15 +5,15 @@ namespace BasicTools.Shared
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class ToolPageAttribute : Attribute
     {
-        public ToolPageAttribute(string name, string description, object faIcon)
+        public ToolPageAttribute(string name, string description, string icon)
         {
             Name = name;
             Description = description;
-            FaIcon = faIcon;
+            Icon = icon;
         }
 
         public string Name { get; }
         public string Description { get; }
-        public object FaIcon { get; }
+        public string Icon { get; }
     }
 }
