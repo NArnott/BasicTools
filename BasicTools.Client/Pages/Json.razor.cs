bd41daeee7436fa19c6b793ad73f08d5255f54eb
Nathan Arnott <nathan@plainlogic.net>
2021-08-08 00:26:05 -0600

    Add Yaml and XML outputs Added Minified outputs

--- BasicTools.Client/Pages/Json.razor.cs
+++ BasicTools.Client/Pages/Json.razor.cs
@@ -1,39 +1,76 @@
 ﻿using System;
-using System.Text.Json;
+using System.ComponentModel;
+using System.Dynamic;
+using Newtonsoft.Json;
 
 namespace BasicTools.Client.Pages
 {
     partial class Json
     {
+        static readonly YamlDotNet.Serialization.Serializer _yaml = new();
+
         string Input { get; set; } = @"{""test"":""value""}";
 
         string Output { get; set; }
 
         bool OutputIsError { get; set; }
 
+        bool WrapOutput { get; set; }
+
+        JsonOutputModes OutputMode { get; set; } = JsonOutputModes.Json;
+
         public void Process()
         {
-            dynamic result;
+            ExpandoObject document = null;
+
+            Output = null;
+            OutputIsError = false;
 
             try
             {
-                result = JsonSerializer.Deserialize<dynamic>(Input);
+                document = JsonConvert.DeserializeObject<ExpandoObject>(Input);
             }
-            catch (Exception ex)
+            catch (JsonSerializationException ex)
+            {
+                Output = ex.Message.Replace("ExpandoObject", "document");
+                OutputIsError = true;
+            }
+            catch(Exception ex)
             {
                 Output = ex.Message;
                 OutputIsError = true;
-                return;
             }
 
-            Output = JsonSerializer.Serialize(
-                result,
-                new JsonSerializerOptions()
+            if (document != null)
+            {
+                Output = OutputMode switch
                 {
-                     WriteIndented = true
-                }
-            );
-            OutputIsError = false;
+                    JsonOutputModes.Json => JsonConvert.SerializeObject(document, Formatting.Indented),
+                    JsonOutputModes.JsonMinified => JsonConvert.SerializeObject(document),
+                    JsonOutputModes.Yaml => _yaml.Serialize(document),
+                    JsonOutputModes.Xml => XmlExpandoSerializer.Serialize(document, System.Xml.Formatting.Indented),
+                    JsonOutputModes.XmlMinified => XmlExpandoSerializer.Serialize(document, System.Xml.Formatting.None),
+                    _ => throw new NotImplementedException(),
+                };
+            }
+
+            WrapOutput = OutputIsError || OutputMode == JsonOutputModes.JsonMinified || OutputMode == JsonOutputModes.XmlMinified;
         }
+    }
+
+    public enum JsonOutputModes
+    {
+        Json,
+
+        [Description("Json (Minified)")]
+        JsonMinified,
+
+        Yaml,
+
+        [Description("XML")]
+        Xml,
+
+        [Description("XML (Minified)")]
+        XmlMinified
     }
 }
---
+++ BasicTools.Client/XmlExpandoSerializer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Dynamic;
+using System.IO;
+using System.Xml;
+
+namespace BasicTools.Client
+{
+    static class XmlExpandoSerializer
+    {
+        public static string Serialize(object obj, Formatting formatting)
+        {
+            using StringWriter stringWriter = new();
+            using XmlTextWriter writer = new(stringWriter)
+            {
+                Formatting = formatting
+            };
+
+            writer.WriteStartDocument();
+
+            SerializeToXml(writer, "XmlDocument", obj);
+
+            writer.WriteEndDocument();
+
+            writer.Flush();
+            stringWriter.Flush();
+
+            return stringWriter.ToString();
+        }
+
+        static void SerializeToXml(XmlWriter writer, ExpandoObject obj)
+        {
+            foreach (var prop in obj)
+            {
+                SerializeToXml(writer, prop.Key, prop.Value);
+            }
+        }
+
+        static void SerializeToXml(XmlWriter writer, string elementName, object obj)
+        {
+            if (obj != null)
+            {
+                switch (obj)
+                {
+                    case ExpandoObject eo:
+                        writer.WriteStartElement(elementName);
+                        SerializeToXml(writer, eo);
+                        writer.WriteEndElement();
+                        break;
+
+                    case string s:
+                        writer.WriteStartElement(elementName);
+                        writer.WriteValue(s);
+                        writer.WriteEndElement();
+                        break;
+
+                    case IEnumerable list:
+                        SerializeToXml(writer, elementName, list);
+                        break;
+                }
+            }
+        }
+
+        static void SerializeToXml(XmlWriter writer, string elementName, IEnumerable list)
+        {
+            foreach (object obj in list)
+            {
+                SerializeToXml(writer, elementName, obj);
+            }
+        }
+
+    }
+}
